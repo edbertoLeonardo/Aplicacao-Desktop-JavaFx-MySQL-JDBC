acf815f99d5ad8d215a099d373cf546552e29df2
Edberto Leonardo <edbleonardo@gmail.com>
2020-02-12 20:12:18 -0300

    Salvando um novo departamento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
 	private Department entity;
+	private DepartmentService departmentService;
 
 	@FXML
 	private TextField textId;
@@ -30,14 +37,40 @@ public class DepartmentFormController implements Initializable {
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onButtonSaveAction() {
-		System.out.println("Click Save");
+	public void setDepartmentService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
 	}
 	
 	@FXML
-	public void onButtonCancelAction() {
-		System.out.println("Click Cancel");
+	public void onButtonSaveAction(ActionEvent actionEvent) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (departmentService == null) {
+			throw new IllegalStateException("DepartmentService was null");
+		}
+		try {
+		entity = getFormData();
+		departmentService.saveOrUpdate(entity);
+		Utils.currentStage(actionEvent).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		
+		Department department = new Department();
+		department.setId(Utils.tryParseToInt(textId.getText()));
+		department.setName(textName.getText());
+		return department;
+		
+	}
+
+	@FXML
+	public void onButtonCancelAction(ActionEvent actionEvent) {
+		Utils.currentStage(actionEvent).close();
 	}
 	
 	@Override
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -7,11 +7,19 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
-	
+
 	private DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-	
-	public List<Department> findAll(){
+
+	public List<Department> findAll() {
 		return departmentDao.findAll();
 	}
 
+	public void saveOrUpdate(Department department) {
+		if (department.getId() == null) {
+			departmentDao.insert(department);
+		} else {
+			departmentDao.update(department);
+		}
+
+	}
 }
